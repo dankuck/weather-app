2f226640d2a4114327c400bd7152adc7960c1991
dankuck <dankuck@gmail.com>
2018-07-05 00:46:24 -0400

    Shuffled tests
    Now App is in charge of retrieving data (root is the best, wooo)
    
    So it gets to have those tests and ForecastList.spec.js is much simpler

--- tests/frontend/App.spec.js
+++ tests/frontend/App.spec.js
@@ -1,22 +1,122 @@
 require('jsdom-global')();
-import { shallowMount } from '@vue/test-utils';
+import { shallowMount, createLocalVue } from '@vue/test-utils';
 import App from '../../frontend/App.vue';
 import Search from '../../frontend/Search.vue';
 import ForecastList from '../../frontend/ForecastList.vue';
 import ForecastDetail from '../../frontend/ForecastDetail.vue';
 import assert from 'assert';
 import expect from 'expect';
+import VueResource from 'vue-resource';
+import VueResourceMocker from 'vue-resource-mocker';
+import sampleResponse from './sample-response.json';
+import waitTicks from './wait-ticks.js';
 
 describe('App', function () {
 
+    let localVue, httpMocker, lastRequest;
+
+    beforeEach(function () {
+        localVue = createLocalVue();
+        localVue.use(VueResource);
+        httpMocker = new VueResourceMocker();
+        localVue.use(httpMocker);
+        httpMocker.setRoutes({
+            GET: {
+                '/api/weather-search': function (request) {
+                    lastRequest = request;
+                    return sampleResponse;
+                },
+            },
+        });
+    });
+
     it('mounts', function () {
-        const wrapper = shallowMount(App, {});
+        const wrapper = shallowMount(App, {localVue});
         assert(wrapper.isVueInstance());
     });
 
     it('has the title', function () {
-        const wrapper = shallowMount(App, {});
+        const wrapper = shallowMount(App, {localVue});
         assert(/Daniel Kuck's Amazing Weather App/.test(wrapper.text()));
+    });
+
+    /**
+     * @LWR 1.d. When the location changes, this component MUST pull new data 
+     * from the backend.
+     */
+    it('pulls from the backend when loaded', function (done) {
+        const wrapper = shallowMount(App, {localVue});
+        assert(lastRequest);
+        assert(/\/api\/weather-search/.test(lastRequest.url));
+        expect(lastRequest.query.location).toEqual('New York');
+        waitTicks(wrapper.vm, 2)
+            .then(() => {
+                expect(wrapper.vm.weather).toEqual(sampleResponse);
+            })
+            .then(done, done);
+    });
+
+    /**
+     * @LWR 1.d. When the location changes, this component MUST pull new data 
+     * from the backend.
+     */
+    it('pulls from the backend when changed', function (done) {
+        const wrapper = shallowMount(App, {localVue});
+        assert(lastRequest); // that's one
+        lastRequest = null;
+        waitTicks(wrapper.vm, 2)
+            .then(() => {
+                wrapper.setData({
+                    searchTerm: 'Boise',
+                });
+                return waitTicks(wrapper.vm, 2);
+            })
+            .then(() => {
+                assert(lastRequest); // the second one!
+                expect(lastRequest.query.location).toEqual('Boise');
+                expect(wrapper.vm.weather).toEqual(sampleResponse);
+            })
+            .then(done, done);
+    });
+
+    /**
+     * @LWR 1.e. When a search is conducted a title header on this section 
+     * MUST display the location found.
+     */
+    it('shows the title', function (done) {
+        const wrapper = shallowMount(App, {localVue});
+        waitTicks(wrapper.vm, 2)
+            .then(() => {
+                assert(/New York/.test(wrapper.text()));
+            })
+            .then(done, done);
+    });
+
+    /**
+     * @LWR 1.e.a. If no matching location is found, the title MAY reflect the 
+     * search bar.
+     *
+     * @LWR 1.e.b. If no matching location is found, a message to that effect 
+     * MUST be shown.
+     */
+    it('falls back to the location and shows an error', function (done) {
+        httpMocker.setRoutes({
+            GET: {
+                '/api/weather-search': function (request) {
+                    return request.respondWith({error: 'No such city'}, {status: 500});
+                },
+            },
+        });
+        const wrapper = shallowMount(App, {localVue});
+        wrapper.setData({
+            searchTerm: 'Trenton',
+        });
+        waitTicks(wrapper.vm, 2)
+            .then(() => {
+                assert(/Trenton/.test(wrapper.text()));
+                assert(/No such city/.test(wrapper.text()));
+            })
+            .then(done, done);
     });
 
     /**
@@ -24,7 +124,7 @@ describe('App', function () {
      * search operation.
      */
     it('the Search component sets the searchTerm', function () {
-        const wrapper = shallowMount(App, {});
+        const wrapper = shallowMount(App, {localVue});
         const search = wrapper.find(Search);
         assert(search.isVueInstance());
         search.vm.$emit('search', 'Des Moines');
@@ -32,34 +132,42 @@ describe('App', function () {
     });
 
     /**
-     * @LWR 2.e. When the location changes, this component MUST pull new data 
+     * @LWR 1.d. When the location changes, this component MUST pull new data 
      * from the backend.
      */
-    it('the App passes the search term to a ForecastList', function () {
-        const wrapper = shallowMount(App, {});
-        const list = wrapper.find(ForecastList);
-        assert(list.isVueInstance());
-        expect(list.vm.location).toEqual('New York');
+    it('the App passes the search term to a ForecastList', function (done) {
+        const wrapper = shallowMount(App, {localVue});
+        waitTicks(wrapper.vm, 2)
+            .then(() => {
+                const list = wrapper.find(ForecastList);
+                assert(list.isVueInstance());
+                expect(list.vm.periods).toEqual(wrapper.vm.weather.list);
+            })
+            .then(done, done);
     });
 
     /**
      * @LWR 2.d. The weather forecast list items MUST display a more detailed 
      * view when clicked.
      */
-    it('the ForecastList component sets the selectedPeriod', function () {
-        const wrapper = shallowMount(App, {});
-        const list = wrapper.find(ForecastList);
-        assert(list.isVueInstance());
-        const period = {};
-        list.vm.$emit('click', period);
-        expect(wrapper.vm.selectedPeriod).toEqual(period);
+    it('the ForecastList component sets the selectedPeriod', function (done) {
+        const wrapper = shallowMount(App, {localVue});
+        waitTicks(wrapper.vm, 2)
+            .then(() => {
+                const list = wrapper.find(ForecastList);
+                assert(list.isVueInstance());
+                const period = {};
+                list.vm.$emit('click', period);
+                expect(wrapper.vm.selectedPeriod).toEqual(period);
+            })
+            .then(done, done);
     });
 
     /**
      * @LWR 2.d.e. The detailed view MUST have an X to close the detailed view.
      */
     it('the ForecastDetail closes', function () {
-        const wrapper = shallowMount(App, {});
+        const wrapper = shallowMount(App, {localVue});
         wrapper.setData({selectedPeriod: {}});
         const detail = wrapper.find(ForecastDetail);
         assert(detail.isVueInstance());
--- tests/frontend/ForecastList.spec.js
+++ tests/frontend/ForecastList.spec.js
@@ -1,151 +1,18 @@
 require('jsdom-global')();
-import { createLocalVue, shallowMount } from '@vue/test-utils';
+import { shallowMount } from '@vue/test-utils';
 import ForecastList from '../../frontend/ForecastList.vue';
 import assert from 'assert';
 import expect from 'expect';
-import VueResource from 'vue-resource';
-import VueResourceMocker from 'vue-resource-mocker';
 import sampleResponse from './sample-response.json';
-import waitTicks from './wait-ticks.js';
 
 describe('ForecastList', function () {
-
-    let localVue, httpMocker;
-
-    beforeEach(function () {
-        localVue = createLocalVue();
-        localVue.use(VueResource);
-        httpMocker = new VueResourceMocker();
-        localVue.use(httpMocker);
-    });
 
     /**
      * @LWR 2. A section MUST exist to display the results of a search.
      */
     it('mounts', function () {
-        const wrapper = shallowMount(ForecastList, {localVue});
+        const wrapper = shallowMount(ForecastList, {});
         assert(wrapper.isVueInstance());
-    });
-
-    /**
-     * @LWR 2.e. When the location changes, this component MUST pull new data 
-     * from the backend.
-     */
-    it('pulls from the backend when loaded', function (done) {
-        let lastRequest;
-        httpMocker.setRoutes({
-            GET: {
-                '/api/weather-search': function (request) {
-                    lastRequest = request;
-                    return sampleResponse;
-                },
-            },
-        });
-        const wrapper = shallowMount(ForecastList, {
-            localVue,
-            propsData: {
-                location: 'Trenton',
-            },
-        });
-        assert(lastRequest);
-        assert(/\/api\/weather-search/.test(lastRequest.url));
-        expect(lastRequest.query.location).toEqual('Trenton');
-        waitTicks(wrapper.vm, 2)
-            .then(() => {
-                expect(wrapper.vm.weather).toEqual(sampleResponse);
-            })
-            .then(done, done);
-    });
-
-    /**
-     * @LWR 2.e. When the location changes, this component MUST pull new data 
-     * from the backend.
-     */
-    it('pulls from the backend when changed', function (done) {
-        let lastRequest;
-        httpMocker.setRoutes({
-            GET: {
-                '/api/weather-search': function (request) {
-                    lastRequest = request;
-                    return sampleResponse;
-                },
-            },
-        });
-        const wrapper = shallowMount(ForecastList, {
-            localVue,
-            propsData: {
-                location: 'Trenton',
-            },
-        });
-        assert(lastRequest); // that's one
-        lastRequest = null;
-        waitTicks(wrapper.vm, 2)
-            .then(() => {
-                wrapper.setProps({location: 'Boise'});
-                return waitTicks(wrapper.vm, 2);
-            })
-            .then(() => {
-                assert(lastRequest); // the second one!
-                expect(lastRequest.query.location).toEqual('Boise');
-                expect(wrapper.vm.weather).toEqual(sampleResponse);
-            })
-            .then(done, done);
-    });
-
-    /**
-     * @LWR 2.b. When a search is conducted a title header on this section 
-     * MUST display the location found.
-     */
-    it('shows the title', function (done) {
-        httpMocker.setRoutes({
-            GET: {
-                '/api/weather-search': function (request) {
-                    return sampleResponse;
-                },
-            },
-        });
-        const wrapper = shallowMount(ForecastList, {
-            localVue,
-            propsData: {
-                location: 'Trenton',
-            },
-        });
-        waitTicks(wrapper.vm, 2)
-            .then(() => {
-                // The sampleResponse gives "New York" even though we asked for "Trenton".
-                // This is useful to test that we show the value from the right place
-                assert(/New York/.test(wrapper.text()));
-            })
-            .then(done, done);
-    });
-
-    /**
-     * @LWR 2.b.a. If no matching location is found, the title MAY reflect the 
-     * search bar.
-     *
-     * @LWR 2.b.b. If no matching location is found, a message to that effect 
-     * MUST be shown.
-     */
-    it('falls back to the location and shows an error', function (done) {
-        httpMocker.setRoutes({
-            GET: {
-                '/api/weather-search': function (request) {
-                    return request.respondWith({error: 'No such city'}, {status: 500});
-                },
-            },
-        });
-        const wrapper = shallowMount(ForecastList, {
-            localVue,
-            propsData: {
-                location: 'Trenton',
-            },
-        });
-        waitTicks(wrapper.vm, 2)
-            .then(() => {
-                assert(/Trenton/.test(wrapper.text()));
-                assert(/No such city/.test(wrapper.text()));
-            })
-            .then(done, done);
     });
 
     /**
@@ -157,53 +24,29 @@ describe('ForecastList', function () {
      * 
      * @LWR 2.c.d. Each list item MUST display a high and low temperature.
      */
-    it('shows the list', function (done) {
-        httpMocker.setRoutes({
-            GET: {
-                '/api/weather-search': function () {
-                    return sampleResponse;
-                },
-            },
-        });
+    it('shows the list', function () {
         const wrapper = shallowMount(ForecastList, {
-            localVue,
             propsData: {
-                location: 'Trenton',
+                periods: sampleResponse.list,
             },
         });
-        waitTicks(wrapper.vm, 2)
-            .then(() => {
-                expect(wrapper.vm.$children.length).toEqual(2);
-            })
-            .then(done, done);
+        expect(wrapper.vm.$children.length).toEqual(2);
     });
-
 
     /**
      * @LWR 2.c.e. Each list item MUST emit when clicked.
      */
-    it('shows the list', function (done) {
-        httpMocker.setRoutes({
-            GET: {
-                '/api/weather-search': function () {
-                    return sampleResponse;
-                },
-            },
-        });
+    it('emits when clicked', function () {
         const wrapper = shallowMount(ForecastList, {
-            localVue,
             propsData: {
-                location: 'Trenton',
+                periods: sampleResponse.list,
             },
         });
-        waitTicks(wrapper.vm, 2)
-            .then(() => {
-                wrapper.vm.$children[0].$emit('click');
-                const clicks = wrapper.emitted().click;
-                assert(clicks);
-                expect(clicks.length).toEqual(1);
-                expect(clicks[0][0].dt_txt).toEqual("2018-07-04 03:00:00");
-            })
-            .then(done, done);
+        
+        wrapper.vm.$children[0].$emit('click');
+        const clicks = wrapper.emitted().click;
+        assert(clicks);
+        expect(clicks.length).toEqual(1);
+        expect(clicks[0][0].dt_txt).toEqual("2018-07-04 03:00:00");
     });
 });
